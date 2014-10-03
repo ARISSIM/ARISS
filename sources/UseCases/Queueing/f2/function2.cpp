60491bef0b7cb5ceed97013e1ab9abdc9b48c3ec
alrinach <ics@Deumo>
2014-10-03 16:48:54 +0200

    Revert "Use case Queueing working : Partition1 sending messages to Partition2"
    This reverts commit 449901f54c4a39b39e05daf67d8b8b718b72c4af.

--- sources/UseCases/Queueing/f2/function2.cpp
+++ sources/UseCases/Queueing/f2/function2.cpp
@@ -1,41 +1,41 @@
 #include "CBasefunction.h"
 #include <iostream>
 int main(int argc, char *argv[]) {
-    int redemarrage = atoi(argv[7]);
-    int position = atoi(argv[6]);
-    GUI_ARINC_partition("Partition2", position, redemarrage);
-    int nbarg = argc;
-    char **argument = new char*[argc];
-    int i = 0;
-    for (i = 0; i <= nbarg; i++) {
-        argument[i] = argv[i];
-    }
-    COMMUNICATION_VECTOR myCvector;
-    myCvector = init_communication(nbarg, argument, NULL);
-    Type_Message rMessage;
-    int portID;
-    int sock;
-    vector_get(&(myCvector.vqueuing_port), 0, &portID);
-    vector_get(&(myCvector.vqueuing_socket), 0, &sock);
-    //    CQueuing Qservice;
-
-    int ifmessage = 0;
-
-    for (;;) {
-        std::cout << "debur boucle for, vqueuing_socket[0] : " << sock << std::endl;
-
-        ifmessage = RECEIVE_QUEUING_MESSAGE(sock, &rMessage);
-        if (ifmessage > 0) {
-            //            Qservice.Display_Message();
-            std::cout << "			" << std::endl;
-            std::cout << "Display message : " << rMessage.m_message << std::endl;
-            std::cout << "length " << rMessage.m_length << std::endl;
-            std::cout << "total length " << sizeof (rMessage) << std::endl;
-            std::cout << "receive :" << rMessage.m_message << std::endl;
-            std::cout << "			" << std::endl;
-        }
-
-    }
+//    int redemarrage = atoi(argv[7]);
+//    int position = atoi(argv[6]);
+//    GUI_ARINC_partition("Partition2", position, redemarrage);
+//    int nbarg = argc;
+//    char **argument = new char*[argc];
+//    int i = 0;
+//    for (i = 0; i <= nbarg; i++) {
+//        argument[i] = argv[i];
+//    }
+//    COMMUNICATION_VECTOR myCvector;
+//    myCvector = init_communication(nbarg, argument, NULL);
+//    Type_Message rMessage;
+//    int portID;
+//    int sock;
+//    vector_get(&(myCvector.vqueuing_port), 0, &portID);
+//    vector_get(&(myCvector.vqueuing_socket), 0, &sock);
+//    //    CQueuing Qservice;
+//
+//    int ifmessage = 0;
+//
+//    for (;;) {
+//        std::cout << "debur boucle for, vqueuing_socket[0] : " << sock << std::endl;
+//
+//        ifmessage = RECEIVE_QUEUING_MESSAGE(sock, &rMessage);
+//        if (ifmessage > 0) {
+//            //            Qservice.Display_Message();
+//            std::cout << "			" << std::endl;
+//            std::cout << "Display message : " << rMessage.m_message << std::endl;
+//            std::cout << "length " << rMessage.m_length << std::endl;
+//            std::cout << "total length " << sizeof (rMessage) << std::endl;
+//            std::cout << "receive :" << rMessage.m_message << std::endl;
+//            std::cout << "			" << std::endl;
+//        }
+//
+//    }
     return 0;
 }
 
